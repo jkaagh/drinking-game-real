fecd3cf42e2176a0af58a94dbdea5a1d38aa3288
jkaagh <69788622+jkaagh@users.noreply.github.com>
2023-01-15 20:31:15 +0100

    asdasd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        {/* <Router>
-            <Routes>
 
-                <Route path="/" component={App}/>
-            </Routes>
-            
-        </Router> */}
-    <App />
+        {/* <KeyboardAwareContainer offset={50} behavior="height"> */}
+
+            <App />
+        {/* </KeyboardAwareContainer> */}
   </React.StrictMode>
 );
 
+
+
+// const KeyboardAwareContainer = ({ children, offset = 0, behavior = 'position' }) => {
+//   const { keyboardHeight, isOpen } = useKeyboard();
+
+//   useEffect(() => {
+//     const container = document.getElementById('keyboard-aware-container');
+//     if (behavior === 'height') {
+//       if (isOpen) {
+//         container.style.height = `calc(100% - ${keyboardHeight}px)`;
+//       } else {
+//         container.style.height = '100%';
+//       }
+//     }
+//     else if (behavior === 'position') {
+//       if (isOpen) {
+//         container.style.position = 'relative';
+//         container.style.bottom = `${keyboardHeight}px`;
+//       } else {
+//         container.style.position = 'initial';
+//         container.style.bottom = 'initial';
+//       }
+//     }
+//   }, [isOpen, keyboardHeight]);
+
+//   return (
+//     <div id='keyboard-aware-container' style={{overflow:'auto'}}>
+//       {children}
+//     </div>
+//   );
+// };
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
